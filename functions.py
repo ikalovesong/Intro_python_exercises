852bf5692dd1c9feaa732144453fe0f723e1d978
Aika Sengirbayeva <ai4h2u2@l-sfo9ebg944-m.gapinc.gap.com>
2016-05-08 12:50:59 -0700

    function execises2

---
+++ functions.py
@@ -0,0 +1,72 @@
+# def find_average(num1, num2) :
+# 	total=num1+num2
+# 	average=total/2
+# 	return average
+
+# print find_average(0,10)
+
+# def hello_print() :
+# 	print "hello"
+
+# print hello_print("hi")
+
+# def hello_return() :
+# 	return "hello"
+
+# greating=hello_return()
+# print greating
+
+# def hello_name(name):
+# 		return "hello" + name
+
+# partners_name=" david"
+# print hello_name(partners_name)
+
+
+
+def add(num1, num2):
+	return num1+num2
+
+def substract(num1, num2):
+	return num1-num2
+
+def multiply(num1, num2):
+	return num1*num2
+
+def devide(num1, num2):
+	return num1/num2
+
+def modulo(num1, num2):
+	return num1%num2
+
+def power(num1, num2):
+	return num1**num2
+
+def square(num1,):
+	return num1*num1
+
+# (4+5) + (9-6)
+# (12/2) - 60
+# 1 + 2 + 3
+# (1 + 2) sqauer
+# (3%4) / 9*9 
+# 7 * (3+8)
+# (1+2) - 3 * (4+5)
+# 3(2+3)
+
+print add(add(4,5), substract(9,6))
+print substract(devide(12,2),60)
+print add(add(1,2),3)
+print square(add(1,2))
+print devide(modulo(3,4), multiply(9,9))
+print multiply(7,add(3,8))
+print substract(add(1,2),multiply(3,add(4,5)))
+
+print power(3, add(2,3))
+
+
+
+
+
+
+
